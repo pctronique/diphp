3d45bd8f7ae87932ac9df4cd6ffbd527e08a4c7f
pctronique <pctronique@free.fr>
2024-05-11 10:17:48 +0200

    rep bug data et simp cron

--- default/image/importdata.sh
+++ default/image/importdata.sh
@@ -2,7 +2,7 @@
 
 if [ -z ${PHP_FOLDER_PROJECT} ]
 then
-    PHP_FOLDER_PROJECT=/usr/local/apache2/www
+    PHP_FOLDER_PROJECT=/usr/local/apache2/www/
 fi
 
 if [ -z ${PHP_FOLDER_LOG} ]
@@ -20,12 +20,10 @@ then
     PHP_FOLDER_INIT_DATA=/docker-entrypoint-initdata.d/
 fi
 
-if [ -d "${PHP_FOLDER_INIT_DATA}" ]; then
-    if [ ! -d "${PHP_FOLDER_PROJECT}${PHP_FOLDER_DATA}" ]; then
-        if [ ! -z "${PHP_FOLDER_INIT_DATA}" ]; then
-            mkdir -p "${PHP_FOLDER_PROJECT}${PHP_FOLDER_DATA}" 2>> ${PHP_FOLDER_LOG}installdata.log
-            cp -r ${PHP_FOLDER_INIT_DATA}* "${PHP_FOLDER_PROJECT}${PHP_FOLDER_DATA}/" 2>> ${PHP_FOLDER_LOG}installdata.log
-        fi
+if [ ! -d "${PHP_FOLDER_PROJECT}/${PHP_FOLDER_DATA}" ]; then
+    if [ ! -z "$(ls -A ${PHP_FOLDER_INIT_DATA})" ]; then
+        mkdir -p "${PHP_FOLDER_PROJECT}/${PHP_FOLDER_DATA}" 2>> ${PHP_FOLDER_LOG}installdata.log
+        cp -r ${PHP_FOLDER_INIT_DATA}* "${PHP_FOLDER_PROJECT}/${PHP_FOLDER_DATA}/" 2>> ${PHP_FOLDER_LOG}installdata.log
     fi
 fi
 
